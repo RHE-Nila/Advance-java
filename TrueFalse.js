c6333c5a17af2a68bcbb5cb160b2ad3554ad4df2
RHE-Nila <61205842+RHE-Nila@users.noreply.github.com>
2020-03-05 19:48:42 +0600

    TrueFalse condition.

---
+++ TrueFalse.js
@@ -0,0 +1,20 @@
+//False:
+//false
+//0
+//undefined
+//null
+//NaN
+//""
+
+//Truthy
+//"0" '0' ' ' " " []
+
+const age = 4;
+//or
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
