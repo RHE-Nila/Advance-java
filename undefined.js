6f0d8408e0bbed27e2cb402adac243554f879781
RHE-Nila <61205842+RHE-Nila@users.noreply.github.com>
2020-03-05 20:17:32 +0600

    undefined condition

---
+++ undefined.js
@@ -0,0 +1,20 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+  console.log(num1 + num2);
+    return
+}
+function add(num1, num2){
+   console.log(num1, num2);
+}
+
+const result = add(13, 82);
+console.log(result);
+
+const premik = {name:"smart dude".phone: 3642346232374};
+console.log(premik.gf);
+let fun = undefined;
+
+let ages = [22, 3, 4]
+console.log(ages[11])
